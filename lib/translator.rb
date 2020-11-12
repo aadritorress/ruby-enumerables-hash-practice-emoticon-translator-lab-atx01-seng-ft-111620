aca4a6c798305ba3ff7fe45be9e6a5f299a6d232
Adri Torres <adriana.amaral@msn.com>
2020-11-12 20:34:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,11 +32,11 @@ end
 def get_english_meaning(path, emoticon)
   emoticons = load_library(path)
   emoticons.each do |meaning, value|
-    # puts value[:japanese] == emoticon
-    if value[:japanese] == emoticon
-      puts meaning[:english]
-    else
-      # puts "Sorry, that emoticon was not found"
-    end
+    puts meaning 
+  #   if value[:japanese] == emoticon
+  #     return meaning[:english]
+  #   else
+  #     puts "Sorry, that emoticon was not found"
+  #   end
   end
 end
