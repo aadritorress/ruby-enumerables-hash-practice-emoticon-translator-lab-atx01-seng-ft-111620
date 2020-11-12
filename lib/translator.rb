ea46ad2e5d74d45e6eba29255076dddd59e6045d
Adri Torres <adriana.amaral@msn.com>
2020-11-12 20:27:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,8 +30,7 @@ end
 
 
 def get_english_meaning(path, emoticon)
-  puts path 
-  # emoticons = load_library(path)
+  emoticons = load_library(path)
   # emoticons.each do |meaning, value|
   #   if value[:japanese] == emoticon
   #     return meaning[:english]
